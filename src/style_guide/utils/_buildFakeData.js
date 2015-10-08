176290e1b0ea9b0f11cf7336dfc5f76a194dff88
nathansmith <nathan@sonspring.com>
2015-10-08 16:22:40 -0500

    Better documenting the `buildFakeData` method.

--- src/style_guide/utils/_buildFakeData.js
+++ src/style_guide/utils/_buildFakeData.js
@@ -1,6 +1,68 @@
 // Dependencies.
 import utils from './index'
 
+/*
+
+//======================================
+// Generates fake data in this format...
+//======================================
+
+{
+  "sortIndex": 0,
+  "sortDirection": "desc",
+  "columns": [
+    {
+      "label": "Date",
+      "type": "date",
+      "sortable": true
+    },
+    {
+      "label": "Description",
+      "sortable": true
+    },
+    {
+      "label": "Category",
+      "sortable": true
+    },
+    {
+      "label": "Amount",
+      "type": "currency",
+      "sortable": true
+    },
+    {
+      "label": "Balance",
+      "type": "currency"
+    }
+  ],
+  "data": [
+    [
+      1444338711008,
+      "Lorem ipsum dolor sit amet...",
+      "Expense",
+      -657.8544315416366,
+      19342.145568458363
+    ],
+    [
+      1444252311008,
+      "Ut enim ad minim veniam...",
+      "Revenue",
+      225.9504753164947,
+      19568.096043774858
+    ],
+    [
+      1444165911008,
+      "Ut enim ad minim veniam...",
+      "Revenue",
+      823.1116528622806,
+      20391.20769663714
+    ]
+
+    // Etc.
+  ]
+}
+
+*/
+
 export default function (count, balance) {
   // Number of rows to generate.
   count = count || 20
