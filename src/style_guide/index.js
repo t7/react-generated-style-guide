272bdb7b4e7168ee340492adc858a246c9dec36f
mundi morgado <mundi@tandemseven.com>
2015-10-11 00:03:36 -0700

    get ready to separate style guide pages from app pages

--- src/style_guide/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOMServer from 'react-dom/server';
-
-function render(Component){
-  return ReactDOMServer.renderToStaticMarkup(<Component />)
-}
-
-export default render
