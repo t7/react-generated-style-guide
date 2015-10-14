a1066713f8579b98a3339767414c351e4024b08a
nathansmith <nathan@sonspring.com>
2015-10-14 13:02:42 -0500

    Uncommenting some paths I was debugging.

--- style_guide/build.js
+++ style_guide/build.js
@@ -1,13 +1,11 @@
 // Require (and run) each "build.js" file.
 
 var pages = [
-  // './pages/branding/build.js',
-  // './pages/intro/build.js',
-  // './pages/patterns/build.js',
-  // './pages/requirements/build.js',
-  // './pages/screens/build.js'
-
-  './pages/patterns/build.js'
+  './pages/branding/build.js',
+  './pages/intro/build.js',
+  './pages/patterns/build.js',
+  './pages/requirements/build.js',
+  './pages/screens/build.js'
 ]
 
 pages.forEach(function (path) {
