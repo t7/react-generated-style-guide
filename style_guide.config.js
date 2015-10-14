11674ce466ff264800b5c7c30cb69c736d8eaa6e
mundi morgado <mundi@tandemseven.com>
2015-10-14 13:14:34 -0700

    initial sg config

---
+++ style_guide.config.js
@@ -0,0 +1,110 @@
+module.exports = {
+  clientName: 'Advisor.Fidelity.com',
+  projectName: 'Interactive Style Guide',
+  colors: {
+    primary: '#FFF',
+    secondary: '#7A8C98',
+    tertiary: '#F5F5F5'
+  },
+  branding: {
+    typography: [
+      {
+        title: 'Lato',
+        family: 'Lato',
+        description: ''
+      },
+      {
+        title: 'Arial, sans-serif',
+        family: 'arial',
+        description: 'Arial font. Fallback to system default sans-serif font if Arial is not available.'
+      }
+    ],
+    colors: [
+      {
+        title: 'Lighter Gray',
+        value: '#F5F5F6',
+        description: 'Right rale light gray'
+      },
+      {
+        title: 'Light Gray',
+        value: '#D4D8DB',
+        description: 'Stroke gray'
+      },
+      {
+        title: 'Middle Gray',
+        value: '#7A8C98',
+        description: 'Topic Header gray'
+      },
+      {
+        title: 'Dark Gray',
+        value: '#4C565E',
+        description: 'Footer/mega menu dark gray'
+      },
+      {
+        title: 'Darkest Gray',
+        value: '#444F57',
+        description: 'Footer/mega menu/body copy darkest gray'
+      },
+      {
+        title: 'Black',
+        value: '#000000',
+        description: 'Headlines'
+      },
+      {
+        title: 'White',
+        value: '#FFFFFF',
+        description: 'Background color'
+      },
+      {
+        title: 'Lighter Blue',
+        value: '#EBF3F9',
+        description: 'Background color'
+      },
+      {
+        title: 'Light Blue',
+        value: '#3A89C3',
+        description: 'Text links/Button'
+      },
+      {
+        title: 'Blue',
+        value: '#44658E',
+        description: 'Equities POV dark blue'
+      },
+      {
+        title: 'Dark Blue',
+        value: '#304969',
+        description: 'Equities POV darkest blue'
+      },
+      {
+        title: 'Green',
+        value: '#5C9631',
+        description: 'Fixed Income POV green'
+      },
+      {
+        title: 'Lime Green',
+        value: '#BBCD3A',
+        description: 'Chart promo lime green'
+      },
+      {
+        title: 'Light Green',
+        value: '#7FB124',
+        description: 'Chart promo green'
+      },
+      {
+        title: 'Dark Green',
+        value: '#4C7D29',
+        description: 'Fixed Income POV darkest green'
+      },
+      {
+        title: 'Orange',
+        value: '#E38B37',
+        description: 'Badges'
+      },
+      {
+        title: 'Red',
+        value: '#D8471C',
+        description: 'Error text'
+      }
+    ]
+  }
+}
