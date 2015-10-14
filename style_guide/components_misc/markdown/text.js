7326bfe14b4d6fcce2fac0d2433ba327634310e6
nathansmith <nathan@sonspring.com>
2015-10-14 14:06:01 -0500

    Adding back in Unsemantic and Markdown helpers, on the "style_guide" and the "source" side.

---
+++ style_guide/components_misc/markdown/text.js
@@ -0,0 +1,38 @@
+// Dependencies.
+import React from 'react'
+
+// Utility methods.
+import utils from '../../utils'
+
+// Define class.
+class Markdown extends React.Component {
+  constructor (props) {
+    // Pass `props` into scope.
+    super(props)
+  }
+
+  // Render method.
+  render () {
+    return (
+      <div
+        dangerouslySetInnerHTML={
+          utils.markdown(
+            require(
+              'raw!../../docs/' +
+              this.props.file.split('.')[0] +
+              '.md'
+            )
+          )
+        }
+      />
+    )
+  }
+}
+
+// Validation.
+Markdown.propTypes = {
+  file: React.PropTypes.string
+}
+
+// Export.
+export default Markdown
