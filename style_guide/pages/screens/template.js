93d7d2d668d2fc4a3fbf283a7a27b4a035c7efe1
mundi morgado <mundi@tandemseven.com>
2015-10-15 07:20:15 -0400

    adding screenshot stuff

--- style_guide/pages/screens/template.js
+++ style_guide/pages/screens/template.js
@@ -33,7 +33,31 @@ class Page extends React.Component {
         </Sidebar>
 
         <Main>
-          Screens Content Here.
+
+          <p className='sg-description'>
+          Templates consist of one or more patterns arranged in a layout. A template can represent a portion of a page or can be a page in and of itself.
+
+          Each template opens in a new browser window.
+          </p>
+
+          {
+            this.screens.forEach(function ({screen, title}, i) {
+              return (
+                <div id='{id}}'>
+                  <div className='sg-title sg-thumbnail-title'>{title}</div>
+                  <div className='sg-thumbnail sg-thumbnail-desktop'>
+                    <a href='{url}' target='_blank'><img src='{src}.html-1366x768.png'/></a>
+                  </div>
+                  <div className='sg-thumbnail sg-thumbnail-tablet'>
+                    <a href='{url}' target='_blank'><img src='{src}.html-768x1024.png'/></a>
+                  </div>
+                  <div className='sg-thumbnail sg-thumbnail-phone'>
+                    <a href='{url}' target='_blank'><img src='{src}.html-320x568.png'/></a>
+                  </div>
+                </div>
+              )
+            })
+          }
         </Main>
 
       </App>
