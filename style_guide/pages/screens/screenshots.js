5ed058cb39c99f5bbfbcd37af441c5d1d78ee644
mundi morgado <mundi@tandemseven.com>
2015-10-16 17:45:58 -0700

    trying out slimer

--- style_guide/pages/screens/screenshots.js
+++ style_guide/pages/screens/screenshots.js
@@ -1,33 +1,8 @@
-var glob = require('glob')
-var Pageres = require('pageres')
-
-var pages = [
-  'http://localhost:8080/',
-  'http://localhost:8080/#/404'
-]
-var currentPageRes
-var numOfPages = pages.length
-
-console.log(numOfPages + ' pages to generate screenshots.')
-function run (page_index) {
-  var url = pages[page_index]
-  console.log('Starting screenshot of ' + url + '......\r')
-  currentPageRes = new Pageres({delay: 1})
-    .src(url, ['1366x768', 'ipad 3', 'iphone 5s'])
-    .dest('build/style_guide/pages/screens/shots')
-    .run(function (err) {
-      if (err) {
-        console.log(err)
-      }
-      console.log('Page (' + (page_index + 1) + ') ' + url + '    ........... Done!          ')
-      page_index = page_index + 1
-      if (pages[page_index]) {
-        setTimeout(function () {
-          currentPageRes = null
-          run(page_index)
-        }, 50)
-      }
-    })
-}
-
-run(0)
+var page = require('webpage').create()
+page.open('http://localhost:8080/#/profile?_k=szuuf4', function (status) {
+  console.log('Status: ' + status)
+  if (status === 'success') {
+    page.render('example.png')
+  }
+  phantom.exit()
+})
