b6d2c5c93a246d3a6b1e93356f64217df827f500
nathansmith <nathan@sonspring.com>
2015-10-17 13:26:38 -0500

    Getting the toggle buttons for patterns/code working.

--- style_guide/pages/patterns/raw_button_toggle.js
+++ style_guide/pages/patterns/raw_button_toggle.js
@@ -1,1 +1,30 @@
+;(function () {
+  var buttons = document.querySelectorAll('[data-code-trigger] button')
 
+  Array.prototype.forEach.call(buttons, function (button) {
+    button.onclick = function (e) {
+      e.preventDefault()
+      e.stopPropagation()
+
+      var p = button.parentNode
+      var id = p.getAttribute('data-code-trigger')
+      var str = '[data-code-example="' + id + '"]'
+      var pre = document.querySelector(str)
+      var hidden = pre.style.display === 'none'
+
+      var state = {
+        true: {
+          display: 'block',
+          text: 'Hide Code'
+        },
+        false: {
+          display: 'none',
+          text: 'Show Code'
+        }
+      }
+
+      button.innerHTML = state[hidden].text
+      pre.style.display = state[hidden].display
+    }
+  })
+})()
