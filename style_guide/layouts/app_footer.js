aa263232654a64c0155ed13b33a66ce7ae9cd1a7
nathansmith <nathan@sonspring.com>
2015-10-19 12:30:18 -0500

    Removing unnnecessary variable. No functional changes.

--- style_guide/layouts/app_footer.js
+++ style_guide/layouts/app_footer.js
@@ -15,15 +15,6 @@ class Footer extends React.Component {
   render () {
     const year = new Date().getFullYear()
 
-    const text = [
-      '©',
-      year,
-      <a href='http://www.tandemseven.com/'>TandemSeven</a>,
-      '—',
-      'View on',
-      <a href='http://github.com/'>GitHub</a>
-    ]
-
     return (
       <footer className={style['isg-app__footer']} role='contentinfo'>
         &copy; {year} <a href='http://www.tandemseven.com/' target='_blank'>TandemSeven</a> &mdash; <a href='http://github.com/' target='_blank'>View on GitHub</a>
