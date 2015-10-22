5c050237cecd5c9f95d1b9167351041d86af12a7
nathansmith <nathan@sonspring.com>
2015-10-22 12:20:41 -0500

    Removing conditional matching for 'shell' since we moved shell.js up a level, out of components.

--- isg/pages/patterns/get_data.js
+++ isg/pages/patterns/get_data.js
@@ -18,57 +18,48 @@ var getData = function (callback) {
     _callback = callback
   }
 
-  if (components[index].match('shell')) {
+  webpackRequire(webpackConfig, require.resolve('../../.' + components[index]), function (error, factory) {
+    if (error) {
+      console.error(error)
+    }
+
+    var jsx = fse.readFileSync(require.resolve('../../.' + components[index]))
+    jsx = jsx.toString().replace(/</g, '&lt;').replace(/>/g, '&gt;')
+
+    var component = factory()
+    var element = React.createElement(component)
+    var string = ReactDOMServer.renderToString(element)
+
+    var markup = ReactDOMServer.renderToStaticMarkup(element)
+    markup = pretty(markup)
+    markup = markup.replace(/\( </g, '(<')
+    markup = markup.replace(/> \)/g, '>)')
+    markup = markup.replace(/</g, '&lt;')
+    markup = markup.replace(/>/g, '&gt;')
+
+    var path = components[index]
+    var id = path.replace('/template.js', '').split('/').pop()
+    var name = id.replace(/_/g, ' ')
+    var url = path.replace('./source', '').replace('template.js', 'index.html')
+
+    data.push({
+      id: id,
+      jsx: jsx,
+      markup: markup,
+      name: name,
+      path: path,
+      string: string,
+      url: url
+    })
+
     index++
+
     if (components[index]) {
       getData()
     } else {
       _callback(data)
     }
-  } else {
-    webpackRequire(webpackConfig, require.resolve('../../.' + components[index]), function (error, factory) {
-      if (error) {
-        console.error(error)
-      }
-
-      var jsx = fse.readFileSync(require.resolve('../../.' + components[index]))
-      jsx = jsx.toString().replace(/</g, '&lt;').replace(/>/g, '&gt;')
-
-      var component = factory()
-      var element = React.createElement(component)
-      var string = ReactDOMServer.renderToString(element)
-
-      var markup = ReactDOMServer.renderToStaticMarkup(element)
-      markup = pretty(markup)
-      markup = markup.replace(/\( </g, '(<')
-      markup = markup.replace(/> \)/g, '>)')
-      markup = markup.replace(/</g, '&lt;')
-      markup = markup.replace(/>/g, '&gt;')
-
-      var path = components[index]
-      var id = path.replace('/template.js', '').split('/').pop()
-      var name = id.replace(/_/g, ' ')
-      var url = path.replace('./source', '').replace('template.js', 'index.html')
-
-      data.push({
-        id: id,
-        jsx: jsx,
-        markup: markup,
-        name: name,
-        path: path,
-        string: string,
-        url: url
-      })
-
-      index++
-
-      if (components[index]) {
-        getData()
-      } else {
-        _callback(data)
-      }
-    })
-  }
+  })
 }
 
 module.exports = getData
