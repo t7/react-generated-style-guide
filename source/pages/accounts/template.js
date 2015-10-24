956179428e61914aa83d585ab40de1605589f939
nathansmith <nathan@sonspring.com>
2015-10-24 10:57:11 -0500

    Verbiage change on "accounts" example page. No functional changes.

--- source/pages/accounts/template.js
+++ source/pages/accounts/template.js
@@ -170,7 +170,7 @@ class Page extends React.Component {
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
               </AccordionPanel>
-              <AccordionPanel label="How much should I save for my children's tuition?">
+              <AccordionPanel label="How much should I save for tuition?">
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
