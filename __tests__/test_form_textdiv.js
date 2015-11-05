c3b8a12a038a32e2fc8c3622b0d77bdb0240f53d
nathansmith <nathan@sonspring.com>
2015-11-05 14:58:37 -0600

    Unit test for <Textdiv/>.

---
+++ __tests__/test_form_textdiv.js
@@ -0,0 +1,98 @@
+/*global
+describe
+expect
+it
+jest
+*/
+
+// Disable auto mocking.
+jest.autoMockOff()
+
+// Dependencies.
+const React = require('react')
+const T = require('react-addons-test-utils')
+
+// UI components.
+const Textdiv =
+require('../source/components/form_textdiv/template')
+
+// Describe `<Component/>` name.
+describe('Textdiv', function () {
+  // Event callback.
+  function handleChange (e, value) {
+    expect(value).toBe('new_value')
+  }
+
+  const disabled = true
+  const id = 'example_id'
+  const name = 'example_name'
+  const placeholder = 'example_placeholder'
+  const required = true
+
+  // Determine pre-filled text.
+  const defaultValue = 'example_value'
+
+  // Insert the component into DOM.
+  const el = T.renderIntoDocument(
+    <Textdiv
+    disabled={disabled}
+    id={id}
+    name={name}
+    placeholder={placeholder}
+    required={required}
+
+    defaultValue={defaultValue}
+
+    handleChange={handleChange}
+    />
+  )
+
+  // Get the `<div>`.
+  const textdiv = T.findRenderedDOMComponentWithTag(el, 'div')
+
+  // ===================
+  // Test for existence.
+  // ===================
+
+  it('exists in the page', function () {
+    expect(T.isCompositeComponent(el)).toBe(true)
+  })
+
+  // =================
+  // Test for textdiv.
+  // =================
+
+  it('is disabled', function () {
+    expect(textdiv.hasAttribute('disabled')).toBe(true)
+  })
+
+  it('has correct ID', function () {
+    expect(textdiv.id).toBe('example_id')
+  })
+
+  it('has correct name', function () {
+    expect(textdiv.getAttribute('name')).toBe('example_name')
+  })
+
+  it('has correct placeholder', function () {
+    const x = textdiv.getAttribute('placeholder')
+
+    expect(x).toBe('example_placeholder')
+  })
+
+  it('is required', function () {
+    expect(textdiv.hasAttribute('required')).toBe(true)
+  })
+
+  it('has correct value', function () {
+    expect(textdiv.innerHTML).toBe('example_value')
+  })
+
+  it('responds to change', function () {
+    T.Simulate.input(textdiv, {
+      target: {
+        innerHTML: 'new_value'
+      }
+    })
+  })
+})
