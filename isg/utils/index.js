f6d887a52adec21e3857cad016822c8ec10ab0c8
nathansmith <nathan@sonspring.com>
2015-12-10 22:42:02 -0600

    Don't parse disabled form elements.

--- isg/utils/index.js
+++ isg/utils/index.js
@@ -1,18 +1,20 @@
 // Import individual files.
 import alphanumeric from './_alphanumeric'
-import convertContentEditable from './_convertContentEditable'
-import convertContentFocus from './_convertContentFocus'
-import convertOnPaste from './_convertOnPaste'
-import convertToMarkup from './_convertToMarkup'
-import convertToText from './_convertToText'
+import convertContentEditable from './_convert_content_editable'
+import convertContentFocus from './_convert_content_focus'
+import convertOnPaste from './_convert_on_paste'
+import convertToMarkup from './_convert_to_markup'
+import convertToText from './_convert_to_text'
 import exists from './_exists'
 import log from './_log'
 import markdown from './_markdown'
 import navigate from './_navigate'
+import parseFormData from './_parse_form_data'
 import placeholder from './_placeholder'
 import regex from './_regex'
 import save from './_save'
 import stop from './_stop'
+import storage from './_storage'
 import title from './_title'
 import today from './_today'
 import trim from './_trim'
@@ -30,10 +32,12 @@ export default {
   log: log,
   markdown: markdown,
   navigate: navigate,
+  parseFormData: parseFormData,
   placeholder: placeholder,
   regex: regex,
   save: save,
   stop: stop,
+  storage: storage,
   title: title,
   today: today,
   trim: trim,
