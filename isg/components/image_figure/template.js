3b34d7dea3a807f5a7b7415eb1d42bf58c0989fe
nathansmith <nathan@sonspring.com>
2016-01-27 19:37:40 -0600

    Fixing a typo.

--- isg/components/image_figure/template.js
+++ isg/components/image_figure/template.js
@@ -5,7 +5,7 @@ import React from 'react'
 import Image from '../image/template'
 
 // CSS.
-import './t7-figure.css'
+import './isg-figure.css'
 
 // Define class.
 class ImageFigure extends React.Component {
@@ -58,7 +58,7 @@ class ImageFigure extends React.Component {
 
     // Expose the UI.
     return (
-      <figure className='t7-figure'>
+      <figure className='isg-figure'>
         {figcaptionTop}
         <Image
           alt={alt}
